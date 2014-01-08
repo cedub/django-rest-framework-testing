0c52fc405359f7719fdd2a9f3aba6d762298343f
Christian West <christianwest@cedub.local>
2014-01-08 13:17:31 -0700

    Revert "Initial commit for the scaffold. Still need to rename some things, but this ought to get us started"
    This reverts commit 3b094b03150336781589ba821b186b48ce6983fb.

--- misc/configs/gunicorn/production.py
+++
@@ -1,8 +0,0 @@
-SITE = 'sbpt_leaderboard'
-
-command = '/opt/{0}/bin/gunicorn'.format(SITE)
-pythonpath = '/opt/{0}/{0}'.format(SITE)
-bind = '127.0.0.1:8002'
-
-preload_app = True
-workers = 6
--- misc/configs/gunicorn/staging.py
+++
@@ -1,7 +0,0 @@
-SITE = 'spbt_leaderboard'
-
-command = '/opt/{0}/bin/gunicorn'.format(SITE)
-pythonpath = '/opt/{0}/{0}'.format(SITE)
-
-preload_app = True
-workers = 1
