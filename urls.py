9e6db640b1dd84e71c5b1e7fa432c3dccca9e7e8
Christian West <christian@meosphere.com>
2014-01-14 13:57:46 -0700

    Adding urls.py and reqs.txt file to scaffold

--- urls.py
+++ urls.py
@@ -5,9 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'spbt_leaderboard.views.home', name='home'),
-    # url(r'^spbt_leaderboard/', include('spbt_leaderboard.foo.urls')),
+    url(r'^$', include('apps.core.urls'), name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
